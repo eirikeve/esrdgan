57eed8a501243fe99d17f399518b268b8a1bf333
Eirik Vesterkjær <eve.eirik@gmail.com>
2019-04-02 15:11:15 +0200

    train loop creates & logs model

--- src/train.py
+++ src/train.py
@@ -11,6 +11,7 @@ import logging
 import random
 
 import torch
+import torch.nn as nn
 
 import config.config as config
 import models.esrdgan as esrdgan
@@ -22,8 +23,16 @@ def train(cfg: config.Config):
     epoch = 0
     it = 0
 
-    gan = esrdgan.ESRDGAN(cfg)
-
+    gan: nn.Module = None
+    if cfg.model.lower() == "esrdgan":
+        status_logger.info(f"run.py: Making model ESRD GAN from config {cfg.name}")
+        gan = esrdgan.ESRDGAN(cfg)
+    else:
+        status_logger.error(f"run.py: Model {cfg.model} is unknown - aborting.")
+        raise NotImplementedError(f"Model {cfg.model} is unknown!")
+    
+    status_logger.info(f"run.py: GAN:\n{str(gan)}\n")
+    log_status_logs(status_logger, gan.get_new_status_logs())
 
     if cfg.training.resume_training_from_save:
         status_logger.info(f"resuming from saves. G: {cfg.env.generator_load_path}, D: {cfg.env.discriminator_load_path}")
@@ -45,4 +54,6 @@ def train(cfg: config.Config):
 def make_loaders():
     return None, None
 
-
+def log_status_logs(status_logger: logging.Logger, logs: list):
+    for log in logs:
+        status_logger.info(log)
