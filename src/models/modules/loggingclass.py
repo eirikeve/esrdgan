dcebfb0e0f9de4e5a34597a790d8af5efa049abe
Eirik Vesterkjær <eve.eirik@gmail.com>
2019-04-03 16:44:12 +0200

    Fix bug which caused clearing not to be global

--- src/models/modules/loggingclass.py
+++ src/models/modules/loggingclass.py
@@ -7,14 +7,16 @@ Implements a base class for storing logs without a logging.Logger
 """
 
 
-class LoggingClass:
+class GlobalLoggingClass:
     """
     LoggingClass has a list of status logs which can be fetched & cleared 
     upon calling get_new_status_logs()
     To make logs, just append to the list
+    Logs are global, so all subclasses log to the same list
     """
     status_logs = []
+
     def get_new_status_logs(self):
         logs = [log for log in self.status_logs]
-        self.status_logs = []
+        self.status_logs.clear()
         return logs        
